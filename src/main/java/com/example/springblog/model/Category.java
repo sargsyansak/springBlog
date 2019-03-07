052f0b098e073aafe67cfdbae51fb9c868754f74
sargsyansak <sargsyansak@gmail.com>
2019-03-08 01:02:19 +0400

    blog with spring

--- src/main/java/com/example/springblog/model/Category.java
+++ src/main/java/com/example/springblog/model/Category.java
@@ -1,23 +1,31 @@
 package com.example.springblog.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
+@EqualsAndHashCode(exclude = "post")
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Entity
 @Table(name = "category")
+@ToString(exclude = "post")
+
 public class Category {
+
+    @ManyToMany(mappedBy = "categories")
+    @Transient
+    Set<Post> post = new HashSet<>();
+
     @Id
-    @Column
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
     private int id;
     @Column
     private String name;
--- src/main/java/com/example/springblog/model/Post.java
+++ src/main/java/com/example/springblog/model/Post.java
@@ -1,22 +1,27 @@
 package com.example.springblog.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
+@EqualsAndHashCode(exclude = "categories")
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Entity
 @Table(name = "post")
+@ToString(exclude = "categories")
+
 
 public class Post {
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(name = "post_category", joinColumns = @JoinColumn(name = "post_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    Set<Category> categories = new HashSet<>();
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,7 +35,6 @@ public class Post {
     private Date date;
     @Column(name = "pic_url")
     private String picUrl;
-    @ManyToOne
-    private Category category;
+
 
 }
